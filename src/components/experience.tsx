a776cfa612dc6394c72a427756272760ca9a71d1
sergi-ss <sergi@thepalace.dev>
2024-03-18 22:41:03 -0230

    stupid deployment

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { Fragment } from 'react'
+import React, { Fragment, ReactNode } from 'react'
 import SectionHeading from './section-heading'
 import { useSectionInView } from '@/lib/hooks'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
@@ -15,30 +15,30 @@ export default function Experience() {
         <section id="experience" ref={ref} className='scroll-mt-28 mb-28 sm:mb-40'>
             <SectionHeading>My Experience</SectionHeading>
             <VerticalTimeline lineColor=''>
-                {
-                    experiencesData.map((item, index) => (
-                        <VerticalTimelineElement
-                            key={index}
-                            contentStyle={{
-                                background: theme === "light" ? "#f3f4f6" : "rgba(255,255,255,0.05)",
-                                boxShadow: "none",
-                                border: "1px solid rgba(0, 0, 0, 0.05)",
-                                textAlign: "left",
-                                padding: "1.3rem 2rem"
-                            }}
-                            contentArrowStyle={{
-                                borderRight: theme === "light" ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255,255,255,0.5)"
-                            }}
-                            date={item.date}
-                            icon={item.icon}
-                            iconStyle={{ background: theme === "light" ? "white" : "rgba(255,255,255,0.5)", fontSize: "1.5rem" }}
-                        >
-                            <h3 className='font-semibold capitalize'>{item.title}</h3>
-                            <p className='font-normal !mt-0 '>{item.location}</p>
-                            <p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>{item.description}</p>
-                        </VerticalTimelineElement>
-                    ))}
+                {experiencesData.map((item, index) => (
+                    <VerticalTimelineElement
+                        key={index}
+                        contentStyle={{
+                            background: theme === "light" ? "#f3f4f6" : "rgba(255,255,255,0.05)",
+                            boxShadow: "none",
+                            border: "1px solid rgba(0, 0, 0, 0.05)",
+                            textAlign: "left",
+                            padding: "1.3rem 2rem"
+                        }}
+                        contentArrowStyle={{
+                            borderRight: theme === "light" ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255,255,255,0.5)"
+                        }}
+                        date={item.date}
+                        icon={item.icon}
+                        iconStyle={{ background: theme === "light" ? "white" : "rgba(255,255,255,0.5)", fontSize: "1.5rem" }}
+                    >
+                        <h3 className='font-semibold capitalize'>{item.title}</h3>
+                        <p className='font-normal !mt-0 '>{item.location}</p>
+                        <p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>{item.description}</p>
+                    </VerticalTimelineElement>
+                )) as ReactNode}
             </VerticalTimeline>
+
 
         </section>
     )
