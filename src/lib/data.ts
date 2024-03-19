3b2fbf434ac0fc66552f06b60fa4305f52792417
sergi-ss <sergi@thepalace.dev>
2024-03-18 23:31:35 -0230

    stupid deployment

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -19,34 +19,34 @@ export const links = [
     name: "Skills",
     hash: "#skills",
   },
-  {
-    name: "Experience",
-    hash: "#experience",
-  },
+  // {
+  //   name: "Experience",
+  //   hash: "#experience",
+  // },
   {
     name: "Contact",
     hash: "#contact",
   },
 ] as const;
 
-export const experiencesData = [
-  {
-    title: "Backend developer Intern",
-    location: "Alex, Egypt",
-    description:
-      "I worked as a backend intern for 3 months. working on a marketplace using OpenCart php.",
-    icon: React.createElement(FaOpencart),
-    date: "6/2022 - 9/2022",
-  },
-  {
-    title: "Backend developer Intern",
-    location: "Alex, Egypt",
-    description:
-      "I'm now a backend developer. My stack includes TypeScript, Prisma and Postgres. occasional involvement in Nextjs, react Native, POS systems.",
-    icon: React.createElement(FaNodeJs),
-    date: "9/2022 - present",
-  },
-];
+// export const experiencesData = [
+//   {
+//     title: "Backend developer Intern",
+//     location: "Alex, Egypt",
+//     description:
+//       "I worked as a backend intern for 3 months. working on a marketplace using OpenCart php.",
+//     icon: React.createElement(FaOpencart),
+//     date: "6/2022 - 9/2022",
+//   },
+//   {
+//     title: "Backend developer Intern",
+//     location: "Alex, Egypt",
+//     description:
+//       "I'm now a backend developer. My stack includes TypeScript, Prisma and Postgres. occasional involvement in Nextjs, react Native, POS systems.",
+//     icon: React.createElement(FaNodeJs),
+//     date: "9/2022 - present",
+//   },
+// ];
 
 export const projectsData = [
   {
