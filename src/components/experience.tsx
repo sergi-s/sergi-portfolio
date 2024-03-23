2d65a91ccf4827790fb755ce564b20780be89296
sergi-ss <sergi@thepalace.dev>
2024-03-23 00:27:39 -0230

    yar rab

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -15,28 +15,31 @@ export default function Experience() {
         <section id="experience" ref={ref} className='scroll-mt-28 mb-28 sm:mb-40'>
             <SectionHeading>My Experience</SectionHeading>
             <VerticalTimeline lineColor=''>
-                {experiencesData.map((experience, index) => (
-                    <VerticalTimelineElement
-                        key={index}
-                        contentStyle={{
-                            background: theme === "light" ? "#f3f4f6" : "rgba(255,255,255,0.05)",
-                            boxShadow: "none",
-                            border: "1px solid rgba(0, 0, 0, 0.05)",
-                            textAlign: "left",
-                            padding: "1.3rem 2rem"
-                        }}
-                        contentArrowStyle={{
-                            borderRight: theme === "light" ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255,255,255,0.5)"
-                        }}
-                        date={experience.date}
-                        icon={experience.icon}
-                        iconStyle={{ background: theme === "light" ? "white" : "rgba(255,255,255,0.5)", fontSize: "1.5rem" }}
-                    >
-                        <h3 className='font-semibold capitalize'>{experience.title}</h3>
-                        <p className='font-normal !mt-0 '>{experience.location}</p>
-                        <p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>{experience.description}</p>
-                    </VerticalTimelineElement>
-                )) as ReactNode}
+                {/* @ts-ignore */}
+                {
+                    // @ts-ignore
+                    experiencesData.map((experience, index) => (
+                        <VerticalTimelineElement
+                            key={index}
+                            contentStyle={{
+                                background: theme === "light" ? "#f3f4f6" : "rgba(255,255,255,0.05)",
+                                boxShadow: "none",
+                                border: "1px solid rgba(0, 0, 0, 0.05)",
+                                textAlign: "left",
+                                padding: "1.3rem 2rem"
+                            }}
+                            contentArrowStyle={{
+                                borderRight: theme === "light" ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255,255,255,0.5)"
+                            }}
+                            date={experience.date}
+                            icon={experience.icon}
+                            iconStyle={{ background: theme === "light" ? "white" : "rgba(255,255,255,0.5)", fontSize: "1.5rem" }}
+                        >
+                            <h3 className='font-semibold capitalize'>{experience.title}</h3>
+                            <p className='font-normal !mt-0 '>{experience.location}</p>
+                            <p className='!mt-1 !font-normal text-gray-700 dark:text-white/75'>{experience.description}</p>
+                        </VerticalTimelineElement>
+                    )) as ReactNode}
             </VerticalTimeline>
 
         </section>
